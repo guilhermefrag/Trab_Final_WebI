8dd34725be5d4bac30035ddb7ef4c0a53e4ec410
guilhermefrag <guilhermefragnani@icloud.com>
2022-06-13 08:49:55 -0300

    Subindo

---
+++ README.md
@@ -0,0 +1,8 @@
+Grupo:
+
+Lucas Baldasar
+Guilherme Fragnani
+
+Referencia:
+
+https://www.youtube.com/watch?v=6BozpmSjk-Y
--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require("path");
 const app = express();
 
 app.use(
-  "/static",
-  express.static(path.resolve(__dirname, "frontend", "static"))
+    "/static",
+    express.static(path.resolve(__dirname, "frontend", "static"))
 );
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
 app.listen(4200, () => console.log("Server running..."));
